3d5ffaff639aa5e16dc5a3a712c5e65b70419a01
Ashutoshkr985 <122661789+Ashutoshkr985@users.noreply.github.com>
2023-03-05 18:43:03 +0530

    Update insertion_sort.js

--- insertion_sort.js
+++ insertion_sort.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   ASHUTOSH KUMAR
     
     *****************
 */
@@ -58,7 +58,7 @@ function Insertion()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   ASHUTOSH KUMAR
     
     *****************
 */
