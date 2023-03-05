163391ebd979b81d82138dc29dda69bf1a88f9f1
Ashutoshkr985 <122661789+Ashutoshkr985@users.noreply.github.com>
2023-03-05 18:44:17 +0530

    Update merge_sort.js

--- merge_sort.js
+++ merge_sort.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-  ASHUTOSH KUMAR
     
     *****************
 */
@@ -75,7 +75,7 @@ function merge_partition(start,end)
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   ASHUTOSH KUMAR
     
     *****************
 */
